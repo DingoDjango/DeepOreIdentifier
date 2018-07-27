f57a743c32c7080afbf0b38403c5ea0fcb767592
Dingo <oferdottan@gmail.com>
2018-07-27 19:27:33 +0300

    Updated Harmony
    Updated 0Harmony.dll and recompiled for latest RimWorld 1.0 Unstable build.

--- Source/OreTextDrawer.cs
+++ Source/OreTextDrawer.cs
@@ -5,9 +5,9 @@ namespace DeepOreIdentifier
 {
 	public class OreTextDrawer : GameComponent
 	{
-		private static readonly float LabelOffsetFromMouse = 2f;
+		private readonly float LabelOffsetFromMouse = 2f;
 
-		private static readonly Color MessageBackgroundColour = new Color(0.15f, 0.15f, 0.15f, 0.8f);
+		private readonly Color MessageBackgroundColour = new Color(0.15f, 0.15f, 0.15f, 0.8f);
 
 		public static bool CheckForDraw;
 
@@ -27,14 +27,14 @@ namespace DeepOreIdentifier
 				string label = ore.label.CapitalizeFirst();
 				Vector2 mousePosition = Event.current.mousePosition;
 				Vector2 labelSize = Text.CalcSize(label);
-				Rect labelRect = new Rect(mousePosition.x - LabelOffsetFromMouse - labelSize.x, mousePosition.y - LabelOffsetFromMouse - labelSize.y, labelSize.x, labelSize.y);
+				Rect labelRect = new Rect(mousePosition.x - this.LabelOffsetFromMouse - labelSize.x, mousePosition.y - this.LabelOffsetFromMouse - labelSize.y, labelSize.x, labelSize.y);
 
 				if (Settings.TextBackground)
 				{
 					Rect backgroundRect = labelRect.ExpandedBy(1f);
 
 					//Verse.Messages.LiveMessage.Draw
-					GUI.color = MessageBackgroundColour;
+					GUI.color = this.MessageBackgroundColour;
 					GUI.DrawTexture(backgroundRect, BaseContent.WhiteTex);
 					GUI.color = Color.white;
 				}
